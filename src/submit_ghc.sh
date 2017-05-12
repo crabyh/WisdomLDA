efe05e97d1fee3ba94056d6148b6261a2410ba85
Chris <crabyh01@gmail.com>
2017-05-12 13:05:14 -0400

    graphs & experiments

--- src/submit_ghc.sh
+++ src/submit_ghc.sh
@@ -1,4 +1,4 @@
 export PATH=$PATH:/usr/lib64/openmpi/bin
 make clean
 make ghc
-mpirun -np 2 ./lda ../nytimes.data ../output 102660 299752 20 0.1 0.1 100 10000 0 0
+mpirun -np 8 ./lda ../nytimes.data ../output 102660 299752 20 0.1 0.1 100 10000 0 0
